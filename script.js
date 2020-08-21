cc5de3908e005f80b5b2b645b5387968042e1709
sbuck0 <buck.samson2000@gmail.com>
2020-08-20 20:50:49 -0400

    Updating changes

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ function sending() {
   console.log("Hello");
   firstname = document.getElementById("firstname").value;
   console.log(firstname);
+  lastname = document.getElementById("lastname").value;
+  console.log(lastname);
+  dob = document.getElementById("dob").value;
+  console.log(dob);
+  date = document.getElementById("date").value;
+  console.log(date);
+  bp = document.getElementById("bp").value;
+  console.log(bp);
+  console.log(firebase.database.ref());
+  // firebase.database().ref("User").set({
+  //   firstname: firstname,
+  //   lastname: lastname,
+  //   dob: dob,
+  //   date: date,
+  //   bp: bp,
+  // });
 }
